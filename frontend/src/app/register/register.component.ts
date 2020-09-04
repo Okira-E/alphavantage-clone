6d6f34be0aeb1fb9be58f9c2d034848fccc49f7e
Okira-E <omarrafat60@gmail.com>
2020-09-04 15:16:32 +0300

    Fixed import path after changing directory name

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,26 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import {NgForm} from "@angular/forms";
-import {UsersService} from "../service/users.service";
+import { NgForm } from '@angular/forms';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
   public token: string;
   public message: string;
 
-  constructor(private userService: UsersService) { }
+  constructor(private userService: UsersService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   registerUser(form: NgForm): void {
     if (!(form.value.email.length === 0)) {
       this.userService.registerUser(form.value.email);
-      this.userService.getToken().subscribe(token => this.token = token);
-      this.userService.getErrorMessage().subscribe(message => {
+      this.userService.getToken().subscribe((token) => (this.token = token));
+      this.userService.getErrorMessage().subscribe((message) => {
         if (message) {
           this.message = message;
         }
