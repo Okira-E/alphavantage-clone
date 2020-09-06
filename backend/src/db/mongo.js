76b47ed8717f8602aa1c1440a004b693a19f2e98
Okira-E <omarrafat60@gmail.com>
2020-09-06 08:56:43 +0300

    Removed the connection from .gitignore

---
+++ backend/src/db/mongo.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose
+    .connect("mongodb://mongo:27017/auth", {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connection with MongoDB has been established!");
+    })
+    .catch(err => {
+        console.log("Couldn't Connect to MongoDB!", err);
+    });
